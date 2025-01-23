d95b65bd80f53fc36e1944107d35d52d4f879aa7
Anggelo <95989098+Anggeloo@users.noreply.github.com>
2025-01-22 22:07:16 -0500

    Code translated to English variables

--- server.js
+++ server.js
@@ -11,37 +11,37 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/products', async (req, res) => {
-  const { nombre, descripcion, categoria, material, color, precio } = req.body;
+  const { name, description, category, material, color, price } = req.body;
 
-  if (!nombre || !precio) {
-    return res.status(400).json(ApiResponse('error', null, 'Debe proporcionar código, nombre y precio.'));
+  if (!name || !price) {
+    return res.status(400).json(ApiResponse('error', null, 'You must provide code, name, and price.'));
   }
 
   try {
 
-    const countQuery = 'SELECT COUNT(*) FROM productos';
+    const countQuery = 'SELECT COUNT(*) FROM products';
     const countResult = await pool.query(countQuery);
 
     const totalProducts = Number(countResult.rows[0].count) + 1;
-  
+
     const query = `
-      INSERT INTO productos (
-        codigo_producto, nombre, descripcion, categoria, material, color, precio
+      INSERT INTO products (
+        product_code, name, description, category, material, color, price
       ) 
       VALUES ($1, $2, $3, $4, $5, $6, $7)
       RETURNING *;
     `;
 
     const params = [
-      `PRO_${totalProducts}`, nombre, descripcion, categoria, material, color, precio
+      `PRO_${totalProducts}`, name, description, category, material, color, price
     ];
 
     const result = await pool.query(query, params);
 
-    res.status(201).json(ApiResponse('success', result.rows[0], 'Producto guardado exitosamente'));
+    res.status(201).json(ApiResponse('success', result.rows[0], 'Product successfully saved'));
   } catch (err) {
     console.log(err);
-    res.status(500).json(ApiResponse('error', null, 'Error al guardar el producto'));
+    res.status(500).json(ApiResponse('error', null, 'Error saving the product'));
   }
 });
 
